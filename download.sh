1c1dae0017ab6b8ed59b1b6b713709907c27a433
Adam Fontenot <adam.m.fontenot@gmail.com>
2024-06-07 17:30:18 -0400

    add download script, remove old leaflet files

---
+++ download.sh
@@ -0,0 +1,2 @@
+wget -O "moments.msgpack?page=1" "https://web.archive.org/web/20241003234934/https://www.queeringthemap.com/moments.msgpack"
+wget "https://web.archive.org/web/20241003234934/https://www.queeringthemap.com/moments.msgpack?page="{2..630}
